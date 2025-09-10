6b83b1e9bad5b8688ec0bfa60fe44d54e21ba98f
mdhanusha25-alt <mdhanusha25@gmail.com>
2025-09-10 17:29:16 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+d={"Eno": 345,"Ename":"abc"}
+print(d)
+print(d["Eno"])
+print(d.get("Ename"))
+d["Enmae"]="xyz"
+print(d)
+d["age"]=19
+print(d)
+d.pop("Eno")
+print(d)
+d.popitem()
+print(d)
+d.clear()
+print(d)
+
+
