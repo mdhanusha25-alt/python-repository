1ed0fc09304698d785e95b52c17ec032a0652333
mdhanusha25-alt <mdhanusha25@gmail.com>
2025-09-19 17:33:56 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,13 @@
+class bill:
+     def __init__(self,a,b,c,d,e)
+         self.sconsumer no=a
+         self.scurrent unit=b
+         self.sname=c
+         self.saddress=d
+         self.sprevious unit=e
+
+     def calculate(self):
+         self.un=self.unit*10
+         self.amount=self.previous unit-self.currentunit
+
+     def    
