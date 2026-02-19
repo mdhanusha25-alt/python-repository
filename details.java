dfe8d92203e911d46d9ec885b275a9d37188bafb
mdhanusha25-alt <mdhanusha25@gmail.com>
2026-02-19 18:06:44 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+public class student{
+  public static void main(String[]args){
+  Scanner sc=new Scanner(System.in);
+    String name;
+    int age;
+    int number;
+    string department;
+ }
+ Student(String name, int age, int number){
+        name=String name;
+        age=int age;
+        number=int number;
+        department=String department;
+    }
+void displayStudentDetails(){
+        System.out.println("Student Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Student Number:"+number);
+        System.out.println("department:"+department);
+}
+ 
+  class mark extends student{
+  int m1,m2m3,tot;
+  float average;
+  string regestation;
+  mark(int m1,int m2,int m3){
+    super(name,age,rollnumber,department
+    mark1=int m1;
+    mark2=int m2;
+    mark3=int m3;
+  }
+public void calculate(){
+  tot=mark1+mark2+mark3;
+  avg=tot/3;
+  if (mark1>40&&mark2>40&&mark3>40){
+    res="pass";
+    else{
+      res="fail";
+    }
+  }
+  public void display(){
+    
+    System.out.println("mark1"+mark1);
+    system.out.println("mark2"+mark2);
+    System.out.println("mark3"+mark3);
+    System.out.println("Total"+tot);
+    System.out.println("Average"+avg);
+    System.out.println("result"+result);
+  }
+}
+public class details{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Name:");
+    string no=sc.next();
+    System.out.println("Enter Department:");
+    String dep=sc.next();
+    System.out.println("Enter Three Subjects Marks;");
+    int mark1=sc.nextint();
+    int mark2=sc.nextint();
+    int mark3=sc.nextint();
+    mark s1=new mark(name,reg,dep,mark1,mark2,mark3)
+    S1.display1;
+    S1.calculate;
+    S1.display2;
+    
+    
+
+      
+ 
+  
+  
+
+
+
+
+
+
