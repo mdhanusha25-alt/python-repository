744566908d18d9f96f61b58866877fface3ffa50
mdhanusha25-alt <mdhanusha25@gmail.com>
2026-02-25 18:08:34 +0530

    Add files via upload

---
+++ sumofdigits.java
@@ -0,0 +1,11 @@
+import java.util.Scanner
+  class number{
+    public static void main(String[],args)
+      Scanner sc=new Scanner(System.in);
+      int n=121;
+      temp=n;
+      rev=0;
+      while(n!=0){
+        rev=rev*10+%10;
+        n=n/10;
+      }
